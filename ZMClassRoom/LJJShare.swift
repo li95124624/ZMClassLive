ff9ac4439c714836fb28c9bb9a09e12578c40132
李建军 <95124624@qq.com>
2020-08-05 16:24:52 +0800

    init commit

---
+++ ZMClassRoom/LJJShare.swift
@@ -0,0 +1,17 @@
+//
+//  LJJShare.swift
+//  tianshi
+//
+//  Created by 李建军 on 2020/8/5.
+//  Copyright © 2020 李建军. All rights reserved.
+//
+
+import Foundation
+
+public class LJJShare {
+    public func LJJShare() {
+        print("come from share....")
+    }
+    
+    public init() {}
+}
